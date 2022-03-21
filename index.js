40fcfcd520ddce645fba0edc56129a20fa69cd77
nakanaka1234 <comomo753@yahoo.co.jp>
2022-03-21 22:56:24 +0900

    必ず数字の値を取るように修正

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ function generatePassword() {
   // console.log(result);
   // console.log(P2);
   let result = '';
+  // creatRandomCharacter(0);
+  console.log(creatRandomCharacter(0));
   for (let i = 0; i < 8; i++) {
-    creatRandomCharacter();
-    result += creatRandomCharacter();
+    result += creatRandomCharacter(null);
   }
   console.log(result);
 
@@ -22,15 +23,26 @@ function generatePassword() {
   password.innerHTML = result;
 }
 
-function creatRandomCharacter() {
+
+function creatRandomCharacter(prm1) {
   const Array = [
     ['1', '2', '3', '4', '5', '6', '7', '8'],
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
     ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
   ];
+  if (prm1 === 0) {
+    return Array[0][Math.floor(Math.random() * 8)];
+    //次回は１が来た時と、２が来た時の条件を追加する
+  } else if (prm1 === null) {
+    const key1 = Math.floor(Math.random() * 3);
+    const key2 = Math.floor(Math.random() * 8);
+    return Array[key1][key2];
+  }
+  console.log(prm1);
   //key1は0〜２のランダムな数　key2は0〜7のランダムな数
   const key1 = Math.floor(Math.random() * 3);
   const key2 = Math.floor(Math.random() * 8);
   return Array[key1][key2];
 }
+
 //8文字（使用はアルファベットの小文字、大文字、数字）をまず１個作成
